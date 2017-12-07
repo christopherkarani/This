fc1a2b5ce4b5acb43c717fa05328dffe6c625bd5
Chris Karani <chrisbkarani@gmail.com>
2017-12-07 14:19:09 +0300

    Initial Commit

---
+++ This/This.swift
@@ -0,0 +1,24 @@
+//
+//  This.swift
+//  This
+//
+//  Created by Christopher Brandon Karani on 07/12/2017.
+//  Copyright © 2017 Christopher Brandon Karani. All rights reserved.
+//
+
+import Foundation
+
+///Conform to this protocol if you want to use This intitilization
+public protocol This {}
+
+extension This {
+    // capture self in a closure and allow user to create object with minimalistic initialization
+    public func this(_ object:  (Self) throws -> Void) rethrows -> Self {
+        try object(self)
+        return self
+    }
+}
+
+extension NSObject: This {}
+
+
