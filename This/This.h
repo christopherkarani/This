ce2dbb8ef6595f82962e690cdb3a1c7d1ce29869
Chris Karani <chrisbkarani@gmail.com>
2017-12-07 10:10:53 +0300

    Initial Commit

---
+++ This/This.h
@@ -0,0 +1,19 @@
+//
+//  This.h
+//  This
+//
+//  Created by Christopher Brandon Karani on 07/12/2017.
+//  Copyright © 2017 Christopher Brandon Karani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for This.
+FOUNDATION_EXPORT double ThisVersionNumber;
+
+//! Project version string for This.
+FOUNDATION_EXPORT const unsigned char ThisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <This/PublicHeader.h>
+
+
